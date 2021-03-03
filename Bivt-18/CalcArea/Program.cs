43cf16f2a66baa1061cfbcee8a461abc382e8ba3
ZorinIvanA <zorin.ivan@outlook.com>
2021-03-03 09:27:02 +0300

    + круг

---
+++ Bivt-18/CalcArea/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CalcArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Выберите фигуру: 1- прямоугольник, 2 - круг");
+                var selectedIndex = int.Parse(Console.ReadLine());
+
+                if (selectedIndex == 1)
+                {
+                    Console.WriteLine("Введите A и B");
+                    var a = float.Parse(Console.ReadLine());
+                    var b = float.Parse(Console.ReadLine());
+                    Console.WriteLine($"Площадь прямоугольника: {a * b}");
+                }
+
+                if (selectedIndex == 2)
+                {
+                    Console.WriteLine("Введите R");
+                    var r = float.Parse(Console.ReadLine());
+                    Console.WriteLine($"Площадь круга: {Math.PI * Math.Pow(r, 2)}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Во время работы произошла ошибка: {e}");
+            }
+        }
+    }
+}
